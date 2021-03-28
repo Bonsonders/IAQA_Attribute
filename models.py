a2bb4a76585b54b1c9f7f8c2024e4d20ce12f83d
Leo Chen <haochen001x@gmail.com>
2021-03-28 14:52:31 +0800

    add models

--- models.py
+++ models.py
@@ -1,2 +1,32 @@
-import torch 
+import torch
 from torch import nn
+import torchvision.models as model_template
+import torch.nn.functional as F
+
+class IAQA_model(nn.Module):
+    def __init__ (self,args):
+        super(IAQA_model,self).__init__()
+        RESNET = model_template.resnet50(pretrained=True)
+        self.features  = nn.Sequential(
+            RESNET.conv1,
+            RESNET.bn1,
+            RESNET.relu,
+            RESNET.maxpool,
+            RESNET.layer1,
+            RESNET.layer2,
+            RESNET.layer3,
+            RESNET.layer4)
+        self.classifer = nn.Sequential(
+            nn.Linear(args.A,args.B),
+            nn.LeakyReLU(True),
+            nn.BatchNorm1d(args.B),
+            nn.Linear(args.B,args.C),
+            nn.LeakyReLU(True),
+            nn.Linear(args.C,1))
+
+    def forward(self,x):
+        x = self.features(x)
+        x = F.max_pool2d(x,(x.size(-2),x.size(-1)))
+        x = self.classifer(x)
+        return x
+
--- train.py
+++ train.py
@@ -1,24 +1,24 @@
-import torch
 import os
+import torch
 from utils.options import TrainOptions
-import numpy as np 
+import numpy as np
 from dataset import get_test_dataloader,get_train_dataloader
 from tensorboardX import SummaryWriter
 from ignite.engine import Events, create_supervised_trainer, create_supervised_evaluator
 from utils.metrics import val_metrics
 from models import *
 from scipy import stats
-
+import adabound
 
 if __name__ == "__main__":
 
     Option = TrainOptions()
     args = Option.parse()
     Option.print_opt()
-    model = None #TODO
+    model = IAQA_model
     device = torch.device("cuda" if args.gpu and torch.cuda.is_available() else "cpu")
     model = model.to(device)
-    optimizer = torch.optim.SGD(model.parameters(),lr = args.lr,momentum = 0.9,weight_decay=5e-4)
+    optimizer = torch.optim.SGD(model.parameters(),lr = args.lr,momentum = 0.9,weight_decay=5e-4) #Change to AdaBound
     val_metric = val_metrics()
     criterion = torch.nn.L1Loss()
     tensorboard_dir = os.path.join(args.runs,args.name)
@@ -27,26 +27,23 @@ if __name__ == "__main__":
     if not os.path.exists(checkpoint_path):
         os.makedirs(checkpoint_path)
     checkpoints = os.path.join(checkpoint_path, '{net}-{epoch}-{type}.pth')
-    
-    
+
+
     train_loader = get_train_dataloader(args)
     val_loader = get_test_dataloader(args)
-    
-    trainer = create_supervised_trainer(model,optimizer,criterion,device = device)
+
+    trainer = create_supervised_trainer(model, optimizer, criterion, device= device)
     best_criterion = 0
     evaluator = create_supervised_evaluator(model,metrics = {'val': val_metric},device = device)
     #writer.add_graph(model)
-    
-    
-    
+
+
+
     @trainer.on(Events.ITERATION_COMPLETED(every = args.log_interval))
     def training_loss(trainer):
         writer.add_scalar("Train/loss", trainer.state.output, trainer.state.iteration)
         print("Epoch[{}] Loss: {:.2f}".format(trainer.state.epoch, trainer.state.output))
 
-    
-    
-    
     @trainer.on(Events.ITERATION_COMPLETED)
     def training_results(trainer):
         evaluator.run(train_loader)
@@ -55,7 +52,7 @@ if __name__ == "__main__":
         print("Training Results - Epoch: {}  Avg accuracy: {:.3f} RMSE: {:.5f}  SROCC: {:.5f} KROCC: {:.5f} PLCC: {:.5f}"
              .format(trainer.state.epoch, Acc, RMSE,SROCC,KROCC,PLCC))
 
-    
+
     @trainer.on(Events.ITERATION_COMPLETED)
     def Validation_results(trainer):
         evaluator.run(val_loader)
@@ -65,7 +62,7 @@ if __name__ == "__main__":
             .format(trainer.state.epoch, Acc, RMSE,SROCC,KROCC,PLCC))
         writer.add_scalar('Test/LOSS', RMSE, trainer.state.epoch)
         writer.add_scalar('Test/SROCC', SROCC, trainer.state.epoch)
-        
+
         if SROCC > best_criterion:
             torch.save(model.state_dict(), checkpoints.format(net=args.name, epoch=trainer.state.epoch, type='best'))
             best_criterion = SROCC
@@ -75,11 +72,11 @@ if __name__ == "__main__":
             labels = list()
             preds = list()
             dis_types = list()
-            for (im,label,dis_type) in val_loader:
+            for (im, label, dis_type) in val_loader:
                 labels.append({dis_type:label})
                 dis_types.append(dis_type)
                 if args.gpu:
-                    im  = im.cuda()
+                    im= im.cuda()
                     label = label.cuda()
                 pred = model(im)
                 preds.append({dis_type:np.asarray(pred)})
@@ -92,10 +89,10 @@ if __name__ == "__main__":
                 writer.add_scalar('{}/LOSS'.format(i),RMSE,trainer.state.epoch)
                 writer.add_scalar('{}/SROCC'.format(i),SROCC,trainer.state.epoch)
                 print("Distortion_Type:{} Test Results - RMSE: {:.5f} SROCC: {:.5f}"
-                    .format(i,RMSE,SROCC))
+                    .format(i, RMSE, SROCC))
 
 
-    trainer.run(train_loader,max_epochs = args.epochs)
+    trainer.run(train_loader, max_epochs= args.epochs)
 
 
 
